cd15aa22d6a95026a30682c6cc5d8f9a04ed244e
Rodrigo Fraga <rodfraga@live.com>
2020-02-09 16:33:50 -0600

    new lessons

---
+++ Learning/Algorithms/MissingElement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Learning.Algorithms
+{
+    public class MissingElement
+    {
+
+        public int Solution(int[] elements)
+        {
+            // edge case
+            if (elements == null || elements.Length == 0)
+                return 0;
+
+            // total number of elements in the series
+            long n = elements.Length + 1;
+
+            // we use the fomula n(n+1)/2 this is the pro one
+            //long series_total = n * (n + 1) / 2;
+            long sum = 0;
+            long series_total = 0;
+            // without the formula
+
+            for (int i = 1; i <= n; i++)
+                series_total += i;
+
+            for (int i = 0; i < n; i++)
+                sum += elements[i];
+            // actual total in the array
+            //long sum = elements.Sum(x => x);
+
+            // difference is the missing number
+
+            return (int)(series_total - sum);
+        }
+
+    }
+
+}
---
+++ Learning/Algorithms/SumToZero.cs
@@ -0,0 +1,36 @@
+﻿namespace Learning.Algorithms
+{
+    public class SumToZero
+    {
+        public void FindSets(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            int[] elements = new int[n];
+
+            int size = n;
+
+            for (int i = 0; i < size / 2; i++)
+            {
+                elements[i] = (i + 1) * -1;
+            }
+
+            if (n % 2 != 0)
+            {
+                for (int i = size / 2; i < size; i++)
+                {
+                    elements[i] = i - (size / 2);
+                }
+            }
+            else
+            {
+                for (int i = size / 2; i < size; i++)
+                {
+                    elements[i] = i - ((size / 2) - 1);
+                }
+            }
+            
+        }
+    }
+}
