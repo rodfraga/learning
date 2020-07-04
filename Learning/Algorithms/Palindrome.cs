e8a9ecf7423c194e39ec954cbe0926cb109c3936
Rodrigo Fraga <rodfraga@live.com>
2020-07-03 23:20:03 -0500

    Update Palindrome.cs

--- Learning/Algorithms/Palindrome.cs
+++ Learning/Algorithms/Palindrome.cs
@@ -4,20 +4,23 @@
     {
         public static bool IsPalindrome(string word)
         {
+            // this regex has been added to make valid the LeetCode Valid Palindrome 
+            // problem wich ignores punctuations and blank spaces
+            Regex rgx = new Regex("[^a-zA-Z0-9]"); 
+            word = rgx.Replace(word, "");
+            
             int min = 0;
             int max = word.Length - 1;
             while (true)
             {
                 if (min > max)
-                {
                     return true;
-                }
+
                 char a = word[min];
                 char b = word[max];
                 if (char.ToLower(a) != char.ToLower(b))
-                {
                     return false;
-                }
+
                 min++;
                 max--;
             }
