df0587ee8ca919b3b8b0dc26fa326148b75cd58d
Rodrigo Fraga <rodfraga@live.com>
2020-07-03 23:00:46 -0500

    Create RemoveDuplicatesFromSortedArray.cs

---
+++ Learning/Algorithms/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,14 @@
+public class RemoveDuplicatesFromSortedArray {
+    public int RemoveDuplicates(int[] nums) {
+        
+        if (nums.Length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.Length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
