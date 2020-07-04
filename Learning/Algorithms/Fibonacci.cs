d2cd3e99fe89317cddcc3afc0cd9f129ba6091cb
Rodrigo Fraga <rodfraga@live.com>
2020-07-03 22:47:12 -0500

    Update Fibonacci.cs

--- Learning/Algorithms/Fibonacci.cs
+++ Learning/Algorithms/Fibonacci.cs
@@ -7,8 +7,12 @@ namespace Learning.Algorithms
         {
             int[] values = new int[n + 1];
 
-            values[0] = 0;
-            values[1] = 1;
+            // Change this as follows to solve the stairs climbing problem for 1 or 2 steps
+            // values[1] = 1
+            // vaklues[2] = 2
+            // and update the start index to; int index = 3 in for loop.
+            values[0] = 0; 
+            values[1] = 1; 
 
             for (int index = 2; index <= n; index++)
                 values[index] = values[index - 1] + values[index - 2];
