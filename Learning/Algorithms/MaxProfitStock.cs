ddebd52654c2fe174a254fab0ab0bbd88c052ad3
Rodrigo Fraga <rodfraga@live.com>
2020-07-09 18:16:01 -0500

    Update MaxProfitStock.cs

--- Learning/Algorithms/MaxProfitStock.cs
+++ Learning/Algorithms/MaxProfitStock.cs
@@ -4,18 +4,33 @@ namespace Learning.Algorithms
 {
     public class MaxProfitStock
     {
-        public int Solution(int[] A)
+        public int Solution(int[] prices)
         {
-            if (A.Length == 0) return 0;
+            if (prices.Length == 0) return 0;
 
-            int min = A[0]; //mainin the previous min price
+            int min = prices[0]; //mainin the previous min price
             int profit = 0; //current max profit
-            for (int i = 1; i < A.Length; i++)
-            {
-                min = Math.Min(min, A[i]); //update minimum
-                profit = Math.Max(profit, A[i] - min); //calculate current profit
+            for (int i = 1; i < prices.Length; i++) {
+                min = Math.Min(min, prices[i]); //update minimum
+                profit = Math.Max(profit, prices[i] - min); //calculate current profit
             }
             return profit > 0 ? profit : 0;
+        }
+        
+        
+        public int SolutionMultipleTransactions(int[] prices)
+        {
+            if(prices == null || prices.Length == 0)
+                return 0;
+            
+            // max profit multiples transactions
+            var diff = 0;
+            for(int i = 1; i < prices.Length; i++) {
+                if(prices[i] > prices[i - 1])
+                    diff += prices[i] - prices[i - 1];
+            }
+
+            return diff < 0 ? 0 : diff;
         }
     }
 }
