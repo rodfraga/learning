6d9fac887785e3821b94aeb86cfb2e999143a147
Rodrigo Fraga <rodfraga@live.com>
2020-12-23 23:20:40 -0600

    Update WordBreak.cs

--- Learning/Algorithms/WordBreak.cs
+++ Learning/Algorithms/WordBreak.cs
@@ -1,1 +1,22 @@
 
+public static bool WordBreak(string s, List<string> words)
+{
+  return WordBreak(s, words, new HashSet<int>(), 0);
+}
+
+public static bool WordBreak(string s, List<string> words, HashSet<int> visited, int index)
+{
+  if(!visited.Add(index))
+    return false;
+    
+    if (index == s.Length)
+      return true;
+      
+    var crop = s.Substring(index);
+    foreach(var word in words) {
+      if(sub.StartsWith(word) && WordBreak(s, words, visited, index + word.Length))
+        return true;
+    }
+    
+    return false;
+}
